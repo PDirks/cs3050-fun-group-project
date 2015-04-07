d68aea2175be537aed9ad88fa14129c93675c216
PDirks- <pld9bc@mail.missouri.edu>
2015-04-07 11:51:21 -0500

    fleshed out classes

--- 3050-group-fun/src/dept.java
+++ 3050-group-fun/src/dept.java
@@ -5,6 +5,29 @@
  * Cody Cameron
  * Peter Dirks
  */
-public class dept {
+import java.util.Scanner;
+import java.util.ArrayList;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class dept {
+	private String name;
+	private ArrayList<applicant> pref;
+	
+	dept( String name, ArrayList<applicant> preferences ){
+		this.name = name;
+		pref = preferences;
+	}
+
+	void addPref( applicant a ){
+		pref.add(a);
+	}
+
+	ArrayList<applicant> getPref(){
+		return pref;
+	}
+	String getName(){
+		return name;
+	}
 }
--- 3050-group-fun/src/main.java
+++ 3050-group-fun/src/main.java
@@ -5,11 +5,21 @@
  * Cody Cameron
  * Peter Dirks
  */
+import java.util.Scanner;
+import java.util.ArrayList;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class main {
 
 	public static void main(String[] args) {
 		
+		System.out.println("program!");
+		reader read = new reader( "test.txt" );
 
+		read.readFile( read.getFile() );
+		
 	}
 
 }
