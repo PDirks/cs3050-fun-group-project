b74080c9a6c2a6456577e12c08d5997bcbf2c30a
PDirks- <pld9bc@mail.missouri.edu>
2015-04-07 11:51:21 -0500

    fleshed out classes

--- 3050-group-fun/src/applicant.java
+++ 3050-group-fun/src/applicant.java
@@ -5,6 +5,31 @@
  * Cody Cameron
  * Peter Dirks
  */
+import java.util.Scanner;
+import java.util.ArrayList;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class applicant {
 
+	private String name;
+	private ArrayList<dept> pref;
+	
+	applicant( String name,ArrayList<dept> preferences){
+		this.name = name;
+		this.pref = preferences;
+	}
+	
+	void addDept( dept d ){
+		pref.add(d);
+	}
+	
+	String getName(){
+		return name;
+	}
+	
+	ArrayList<dept> getPref(){
+		return pref;
+	}
 }
